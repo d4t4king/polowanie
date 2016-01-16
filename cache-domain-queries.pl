e77cd25296743450a7b71682d0fac430786431e3
dataking <dataking@gmail.com>
2016-01-16 13:51:19 -0800

    Added depth, threashold, whitelist, help, show-mime-types.

--- cache-domain-queries.pl
+++ cache-domain-queries.pl
@@ -7,18 +7,29 @@ use Term::ANSIColor;
 use Data::Dumper;
 use Getopt::Long;
 
-my ($help, $verbose, $depth, $threshold);
+my ($help, $verbose, $depth, $threshold, $show_tlds, $whitelist, $show_mime_types);
 $depth = 10;
 $threshold = 10;
 $verbose = 0;
 $help = 0;
+$show_tlds = 0;
+$show_mime_types = 0;
 GetOptions(
-	"h|?|help"	=>	\$help,
-	"v|verbose+"	=>	\$verbose,
-	"d|depth=s"	=>	\$depth,
+	"h|?|help"			=>	\$help,
+	"v|verbose+"		=>	\$verbose,
+	"d|depth=s"			=>	\$depth,
+	"t|threshold=s"		=>	\$threshold,
+	"show-tlds"			=>	\$show_tlds,
+	"show-mime-types"	=>	\$show_mime_types,
+	"w|whitelist=s"		=>	\$whitelist,
 );
 
-my (%tlds, %ttdomains, %domains, %mime_types);
+my (%tlds, %ttdomains, %domains, %mime_types, %whitelist);
+
+if ($help) { &show_help(); }
+
+if ($whitelist) { %whitelist = &load_whitelist($whitelist); }
+
 if ((defined($ARGV[0])) && ($ARGV[0] ne "")) {
 	if (( -e $ARGV[0] ) && (! -z $ARGV[0])) {
 		open IN, $ARGV[0] or die "Couldn't open input file ($ARGV[0]): $! \n";
@@ -38,7 +49,7 @@ if ((defined($ARGV[0])) && ($ARGV[0] ne "")) {
 					if (($tld =~ /^\d+$/) && (($tld >= 0) && ($tld <= 255))) {
 						#print STDERR "Likely an unresolved IP. \n";		# deal with this later
 					} else {
-						if ($verbose && $verbose < 1) {
+						if ($verbose) {
 							warn colored("Unexpected characters in TLD.", "bold yellow");
 							print STDERR colored("URL=$url \n", "yellow");
 							print STDERR colored("TLD=$tld \n", "yellow");
@@ -70,14 +81,76 @@ if ((defined($ARGV[0])) && ($ARGV[0] ne "")) {
 }
 
 my $i = 0;
-print colored("Found the following unique top-level domains: \n", "bold cyan");
-foreach my $t ( sort { $tlds{$a} <=> $tlds{$b} } keys %tlds ) {
-	next unless ($tlds{$t} <= $threshold);
-	printf "%32s %-9d \n", $t, $tlds{$t};
+if ($show_tlds) {
+	print colored("Found the following unique top-level domains: \n", "bold cyan");
+	foreach my $t ( sort { $tlds{$a} <=> $tlds{$b} } keys %tlds ) {
+		if ($threshold) { next unless ($tlds{$t} <= $threshold); }
+		printf "%32s %-9d \n", $t, $tlds{$t};
+		last if (($depth) && ($i == $depth));
+		$i++;
+	}
 }
+
+$i = 0;
+if ($show_mime_types) {
+	print colored("Found the following unique mime types: \n", "bold cyan");
+	foreach my $mt ( sort { $mime_types{$a} <=> $mime_types{$b} } keys %mime_types ) {
+		if ($threshold) { next unless ($mime_types{$mt} <= $threshold); }
+		printf "%32s %-9d \n", $mt, $mime_types{$mt};
+		last if (($depth) && ($i == $depth));
+		$i++;
+	}
+}
+
+$i = 0;
 print colored("Found the following unique primary domains: \n", "bold cyan");
 foreach my $t ( sort { $ttdomains{$a} <=> $ttdomains{$b} } keys %ttdomains ) {
-	next unless ($ttdomains{$t} <= $threshold);
+	if ($threshold) { next unless ($ttdomains{$t} <= $threshold); }
+	if ($whitelist) { next if (exists($whitelist{$t})); }
 	printf "%32s %-9d \n", $t, $ttdomains{$t};
+	last if (($depth) && ($i == $depth));
+	$i++;
+}
+
+###############################################################################
+###		Subs
+###############################################################################
+sub show_help() {
+	print <<EoS;
+
+$0 [-h|--help] [-v|--verbose] [-d|--depth] <integer> [-t|--threshold] <integer> [-w|--whitelist] </path/to/whitelist> --show-tlds
+
+Where:
+
+-h|--help			Displays this helpful message.
+-v|--verbose		Shows extra output messages.  Specify more times to increase verbosity.
+-d|--depth			Specify the Top/Bottom number of results.
+-t|--threshold		Specify all results above/below the specified repetition count.
+-w|--whitelist		Specifies a list of accepted domains to ignore.  File should be
+					one entry per line.
+--show-tlds			Show TLDs within specified/default depth/threshold, in addition to the
+					primary domains found.
+
+EoS
+
+	exit 0;
+}
+
+sub load_whitelist() {
+	my $wl = shift(@_);
+	my %wl;
+
+	if ((-e $wl) && (!-z $wl)) {
+		open WL, $wl or die colored("Couldn't open whitelist file ($wl) for processing: $! \n", "bold red");
+		while (my $line = <WL>) {
+			chomp($line);
+			$wl{$line}++;
+		}
+		close WL or die colored("Coudln't close whitelist file ($wl): $! \n", "bold red");
+	} else {
+		die colored("There was a problem with the whitelist file ($wl).  It does not exist or is zero bytes. \n", "bold red");
+	}
+
+	return %wl;
 }
 
