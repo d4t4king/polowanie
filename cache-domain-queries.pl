d31c6f5ba1cbec6fad4a03c810c7eca2d65ecd79
dataking <dataking@gmail.com>
2016-01-17 00:33:19 -0800

    Tweaked output to match style, for uniformtiy.

--- cache-domain-queries.pl
+++ cache-domain-queries.pl
@@ -32,7 +32,7 @@ if ($whitelist) { %whitelist = &load_whitelist($whitelist); }
 
 if ((defined($ARGV[0])) && ($ARGV[0] ne "")) {
 	if (( -e $ARGV[0] ) && (! -z $ARGV[0])) {
-		open IN, $ARGV[0] or die "Couldn't open input file ($ARGV[0]): $! \n";
+		open IN, $ARGV[0] or die colored("[EE] Couldn't open input file ($ARGV[0]): $! \n", "bold red");
 		while (my $line = <IN>) {
 			chomp($line);
 			#0397706.395     40 192.168.1.10 TCP_MISS/202 478 POST http://telemetry.battle.net/api/submit - ORIGINAL_DST/24.105.29.23 application/json
@@ -50,9 +50,9 @@ if ((defined($ARGV[0])) && ($ARGV[0] ne "")) {
 						#print STDERR "Likely an unresolved IP. \n";		# deal with this later
 					} else {
 						if ($verbose) {
-							warn colored("Unexpected characters in TLD.", "bold yellow");
-							print STDERR colored("URL=$url \n", "yellow");
-							print STDERR colored("TLD=$tld \n", "yellow");
+							warn colored("[!!] Unexpected characters in TLD.", "bold yellow");
+							print STDERR colored("[%%] URL=$url \n", "yellow");
+							print STDERR colored("[%%] TLD=$tld \n", "yellow");
 						}
 					}
 				} else {
@@ -61,7 +61,7 @@ if ((defined($ARGV[0])) && ($ARGV[0] ne "")) {
 						if ((defined($parts[-2])) && ($parts[-2] ne '')) {
 							$ttdomains{"$parts[-2].$parts[-1]"}++;
 						} else {
-							print STDERR colored("Part 2 missing or blank: $line \n", "bold yellow") if ($verbose);
+							print STDERR colored("[!!] Part 2 missing or blank: $line \n", "bold yellow") if ($verbose);
 						}
 					}
 				}
@@ -70,22 +70,22 @@ if ((defined($ARGV[0])) && ($ARGV[0] ne "")) {
 			} elsif ($url =~ /^error:invalid-request/) {
 				# still do nothing, but this will be interesting later, maybe
 			} else { 
-				if ($verbose) { warn colored("URL didn't match domain regex: $url \n", "bold yellow"); }
+				if ($verbose) { warn colored("[!!] URL didn't match domain regex: $url \n", "bold yellow"); }
 			}
 		}
 	} else {
-		die colored("There was a problem with the input file ($ARGV[0]): $! \n", "bold red");
+		die colored("[EE] There was a problem with the input file ($ARGV[0]): $! \n", "bold red");
 	}
 } else {
-	die colored("You need to specify a cache log to parse as an argument! \n", "bold red");
+	die colored("[EE] You need to specify a cache log to parse as an argument! \n", "bold red");
 }
 
 my $i = 0;
 if ($show_tlds) {
-	print colored("Found the following unique top-level domains: \n", "bold cyan");
+	print colored("[**] Found the following unique top-level domains: \n", "bold cyan");
 	foreach my $t ( sort { $tlds{$a} <=> $tlds{$b} } keys %tlds ) {
 		if ($threshold) { next unless ($tlds{$t} <= $threshold); }
-		printf "%32s %-9d \n", $t, $tlds{$t};
+		printf "[**] %32s %-9d \n", $t, $tlds{$t};
 		last if (($depth) && ($i == $depth));
 		$i++;
 	}
@@ -93,21 +93,21 @@ if ($show_tlds) {
 
 $i = 0;
 if ($show_mime_types) {
-	print colored("Found the following unique mime types: \n", "bold cyan");
+	print colored("[**] Found the following unique mime types: \n", "bold cyan");
 	foreach my $mt ( sort { $mime_types{$a} <=> $mime_types{$b} } keys %mime_types ) {
 		if ($threshold) { next unless ($mime_types{$mt} <= $threshold); }
-		printf "%32s %-9d \n", $mt, $mime_types{$mt};
+		printf "[**] %32s %-9d \n", $mt, $mime_types{$mt};
 		last if (($depth) && ($i == $depth));
 		$i++;
 	}
 }
 
 $i = 0;
-print colored("Found the following unique primary domains: \n", "bold cyan");
+print colored("[**] Found the following unique primary domains: \n", "bold cyan");
 foreach my $t ( sort { $ttdomains{$a} <=> $ttdomains{$b} } keys %ttdomains ) {
 	if ($threshold) { next unless ($ttdomains{$t} <= $threshold); }
 	if ($whitelist) { next if (exists($whitelist{$t})); }
-	printf "%32s %-9d \n", $t, $ttdomains{$t};
+	printf "[**] %32s %-9d \n", $t, $ttdomains{$t};
 	last if (($depth) && ($i == $depth));
 	$i++;
 }
