58ea6d699132d69f1ee0fb456cc628fe7902f0ee
dataking <dataking@gmail.com>
2016-11-01 21:12:54 -0700

    adding length analysis

--- packet-size.pl
+++ packet-size.pl
@@ -22,14 +22,19 @@ GetOptions(
 );
 
 my $not_syn = 0;
+#my $syns = 0;
 my $total_packets = 0;
 my $first = 0;
 my $last = 0;
 my $time_diff = 0;
-my %seqs;
+my (%seqs, %lengths, %len_srcs, %len_dsts);
+
+&show_help if ($help);
 
 if (($tcp) && ($udp)) {
-	die colored("Can't operate on both TCP and UDP at this point in time. \n", "bold red");
+	#die colored("Can't operate on both TCP and UDP at this point in time. \n", "bold red");
+	print "It is the default behavior to process both TCP and UDP packets.  Ignoring.\n";
+	$tcp = undef; $udp = undef;
 }
 
 my $count = 0;
@@ -43,6 +48,7 @@ while (my $line = <IN>) {
 	if ($tcp) { next unless ($line =~ /PROTO=TCP/); }
 	$total_packets++;
 	if ($line =~ / SYN /) {
+		#$syns++;
 		#print "$line\n";
 		my @parts = split(/\s+/, $line);
 		my $epoch_date = &get_epoch_date($parts[0], $parts[1], $parts[2]);
@@ -55,6 +61,15 @@ while (my $line = <IN>) {
 		if ($line =~ /LEN=([0-9]+)/) { $len = $1; } 
 		else { warn colored("Couldn't match packet length in line! \n", "yellow"); }
 		$seqs{$srcip}{$dstip}{$epoch_date} = $len;
+		if (exists($lengths{$len}{$srcip}{$dstip})) { 
+			$lengths{$len}{$srcip}{$dstip}++; 
+			$len_srcs{$len}{$srcip}++;
+			$len_dsts{$len}{$dstip}++;
+		} else { 
+			$lengths{$len}{$srcip}{$dstip} = 0; 
+			$len_srcs{$len}{$srcip} = 0;
+			$len_dsts{$len}{$dstip} = 0;
+		}
 		$count++;
 		$last = $epoch_date;
 	} else { $not_syn++; }
@@ -83,18 +98,47 @@ END {
 	print colored("F: $first L: $last \n", "bold yellow");
 	print colored("F: $fm/$fd/$fy $fH:$fM:$fS L: $lm/$ld/$ly $lH:$lM:$lS \n", "bold yellow");
 	print colored("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n", "bold cyan");
-	print colored("=-\tTotal packets encountered after filter: $total_packets \n", "bold cyan");
-	print colored("=-\tBlocked Non-SYN packets: $not_syn \n", "bold cyan");
-	print colored("=-\tTime different between first and last: $time_diff secs \n", "bold cyan");
+	print colored("=-\tTotal packets encountered after filter: ".&commify($total_packets)." \n", "bold cyan");
+	print colored("=-\tBlocked SYN packets: ".&commify($count)." \n", "bold cyan");
+	print colored("=-\tBlocked Non-SYN packets: ".&commify($not_syn)." \n", "bold cyan");
+	print colored("=-\tSanity check: total packets - non-SYN packets = SYNs:\n", "bold cyan");
+	print colored("=-\t              ".&commify($total_packets)." - ".&commify($not_syn)." = ".&commify(($total_packets - $not_syn))."\n", "bold cyan");
+	print colored("=-\tTime different between first and last: ".&commify($time_diff)." secs \n", "bold cyan");
 	print colored("=-\tTime different between first and last: $days days, $hours hours, $mins mins, $secs secs \n", "bold cyan");
 	print colored("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n", "bold cyan");
+	#print Dumper(\%lengths);
+	foreach my $len ( sort { $lengths{$b} <=> $lengths{$a} } keys %lengths ) {
+		print "$len: ".scalar(keys(%{$len_srcs{$len}}))."\n";
+	}
+	foreach my $len ( sort { $lengths{$b} <=> $lengths{$a} } keys %lengths ) {
+		print "$len: ".scalar(keys(%{$len_dsts{$len}}))."\n";
+	}
 }
 
 ###############################################################################
 ### Subroutines
 ###############################################################################
 sub show_help() {
+	print <<END;
 
+Usage: $0 [-h] [-v] [--tcp] [--udp]
+
+Where:
+
+-h|--help					Displays this useful message then exits.
+-v|--verbose			Displays more output.  Usually used for debugging.
+--tcp						Analyzes only log entries for the TCP protocol
+--udp						Analyzes only log entries for the UDP protocol
+
+END
+	exit 0;
+
+}
+
+sub commify {
+	my $text = reverse $_[0];
+	$text =~ s/(\d\d\d)(?=\d)(?!\d*\.)/$1,/g;
+	return scalar reverse $text;
 }
 
 sub get_epoch_date() {
